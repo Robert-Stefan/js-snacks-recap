b4e21741718c0b4f46d0517b0dbececa73412c25
Robert-Stefan <robertboss05@hotmail.it>
2021-05-01 18:54:35 +0200

    Add snack 2

--- snackspomeridiani/parte3/js/script.js
+++ snackspomeridiani/parte3/js/script.js
@@ -4,7 +4,7 @@
     Es: ['pippo', 'PLUTO', 'Paperino'] => ['Pippo', 'Pluto', 'Paperino']
 */
 
-const nomi = ['pippo', 'PLUTO', 'Paperino'];
+/*const nomi = ['pippo', 'PLUTO', 'Paperino'];
 
 console.log(nomi);
 
@@ -12,4 +12,30 @@ const newFormat = nomi.map((element, index) => {
     return element.charAt(0).toUpperCase() + nomi[index].slice(1).toLowerCase()
 });
 
-console.log(newFormat);
+console.log(newFormat);*/
+
+
+/*
+    SNACK 2
+    Un oggetto rappresenta una finestra di un browser e contiene due campi: 
+    un array di tab aperte
+    un numero che indica l'indice della tab aperta nell'array:
+    {
+    "tab": ["Facebook", "GitHub", "Gmail"],
+    "activeTab": 0
+    }
+    Il software deve guardare se c'è un social aperto ed eliminarlo dalle tab.
+    Nel caso la tab fosse attiva, deve attivare la successiva.
+*/
+
+const browser = {
+    tab: ["Facebook", "GitHub", "Gmail"],
+    activeTab: 0,
+};
+
+const filtered = browser.tab.filter((tab,index) => index == browser.activeTab + 1);
+
+
+console.log(browser);
+console.log(filtered);
+
