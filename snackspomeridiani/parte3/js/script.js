1be57695b48506afaaa3d930d40c8ec2742398f5
Robert-Stefan <robertboss05@hotmail.it>
2021-05-01 19:05:37 +0200

    Add snack 3

--- snackspomeridiani/parte3/js/script.js
+++ snackspomeridiani/parte3/js/script.js
@@ -28,7 +28,7 @@ console.log(newFormat);*/
     Nel caso la tab fosse attiva, deve attivare la successiva.
 */
 
-const browser = {
+/*const browser = {
     tab: ["Facebook", "GitHub", "Gmail"],
     activeTab: 0,
 };
@@ -37,5 +37,18 @@ const filtered = browser.tab.filter((tab,index) => index == browser.activeTab + 
 
 
 console.log(browser);
-console.log(filtered);
+console.log(filtered);*/
 
+/*
+    SNACK 3  -> si potrebbe ottimizzare con una funzione separata per il capitalize
+    A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l'iniziale maiuscola.
+    Es: ['pippo', 'PLUTO', 'Paperino'] => ['Pippo', 'Pluto', 'Paperino']
+*/
+
+/*const nomi = ['pippo', 'PLUTO', 'Paperino'];
+
+console.log(nomi);
+
+const newFormat = nomi.map((element, index) => `${element.charAt(0).toUpperCase()}${nomi[index].slice(1).toLowerCase()}`);
+
+console.log(newFormat);*/
